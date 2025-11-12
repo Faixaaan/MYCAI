087991a53970ac0ac6f02e8edbe3707c5cbf53b3
Faizan <mandrws.shadow@gamil.com>
2025-11-12 13:31:12 +0530

    remove unused variables

--- src/Pages/MyCv/index.jsx
+++ src/Pages/MyCv/index.jsx
@@ -3,7 +3,7 @@ import Banner from './components/Banner'
 import Chatbot from '../Home/Components/Chatbot'
 import BetterCV from './components/BetterCV'
 import CvBuilder from './components/CvBuilder'
-import Customers from './components/Customers'
+
 
 const MyCv = () => {
   return (
